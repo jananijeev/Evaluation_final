63f3e1cb7a6aff19bdaa5cac4719d0a9b8db120f
jananijeev <jananij0015@gmail.com>
2024-02-19 11:49:01 +0530

    second commit

--- add.c
+++ add.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main() {
+int main()
+ {
     int num1, num2, sum;
     
     printf("Enter the first number: ");
@@ -13,5 +14,5 @@ int main() {
     
     printf("The sum of %d and %d is %d.", num1, num2, sum);
     
-    return 0;
+    return 0
 }
---
+++ mul.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+  int multiplication = 0;
+  int number1 = 100, number2 = 2;
+  multiplication= number1 * number2;
+  printf("Multiplication = %d\n",  multiplication);
+  return 0;
+}
