388147bb75e7390eb1777864a91bccbbff49b771
Jananijeev <155942859+jananijeev@users.noreply.github.com>
2024-02-19 12:05:33 +0530

    Update add.c

--- add.c
+++ add.c
@@ -8,11 +8,11 @@ int main()
     scanf("%d", &num1);
     
     printf("Enter the second number: ");
-    scanf("%d", &num2)
+    scanf("%d", &num2);
     
     sum = num1 + num2;
     
     printf("The sum of %d and %d is %d.", num1, num2, sum);
     
-    return 0;
+    return 0
 }
