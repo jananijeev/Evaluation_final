30d2cb4426ed3e501bd23234380a9300e804a025
Jananijeev <155942859+jananijeev@users.noreply.github.com>
2024-02-19 11:27:37 +0530

    Update add.c

--- add.c
+++ add.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int num1, num2, sum;
     
     printf("Enter the first number: ");
     scanf("%d", &num1);
     
     printf("Enter the second number: ");
-    scanf("%d", &num2);
+    scanf("%d", &num2)
     
     sum = num1 + num2;
     
